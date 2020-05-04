6b74701d53557383de0997e008451a90cd51e5f0
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-04 16:56:35 -0300

    feat: repository route

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import { Title, Form, Repositories, Error } from './styles';
@@ -16,7 +17,23 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storegedRepositories = localStorage.getItem(
+            '@GithubExplorer:repositories',
+        );
+
+        if (storegedRepositories) {
+            return JSON.parse(storegedRepositories);
+        }
+        return [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExplorer:repositories',
+            JSON.stringify(repositories),
+        );
+    }, [repositories]);
 
     async function handleAddRepository(
         event: FormEvent<HTMLFormElement>,
@@ -56,7 +73,10 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map((repository) => (
-                    <a key={repository.full_name} href="teste">
+                    <Link
+                        key={repository.full_name}
+                        to={`/repository/${repository.full_name}`}
+                    >
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -67,7 +87,7 @@ const Dashboard: React.FC = () => {
                         </div>
 
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
